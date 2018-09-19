fea7ebfd8d4e4bc070679bbc22499013d58fccc0
Lora Kisyova <llovchalieva@gmail.com>
2018-09-19 14:49:01 +0300

    Basic app with some enhancements

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
-import Navigation from './components/Navigation';
-
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Particles from 'react-particles-js'
+const particlesOptions = {
+  particles: {
+      number: {
+        value: 80,
+        density: {
+          enable: true,
+          value_area: 400
+        }
+      }
+  }
+};
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Navigation/>
-      {/*<Logo />
-      <ImageLinkForm />
-      <FaceRecognition />*/}
+        <Particles className="particles" 
+          params={particlesOptions}
+        />
+        <Navigation/>
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/*<FaceRecognition />*/}
       </div>
     );
   }
