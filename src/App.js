3d5fe4048af1219f917e9dfab883d155b2ae3195
Lora Kisyova <llovchalieva@gmail.com>
2018-10-22 14:14:54 +0300

    Add dummy function interactions

--- src/App.js
+++ src/App.js
@@ -17,6 +17,20 @@ const particlesOptions = {
   }
 };
 class App extends Component {
+  constructor()  {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log("Click");
+  }
   render() {
     return (
       <div className="App">
@@ -26,11 +40,15 @@ class App extends Component {
         <Navigation/>
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/*<FaceRecognition />*/}
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}
+        />
+          {/*<FaceRecognition />*/}
       </div>
     );
   }
 }
+
 
 export default App;
