e54f54d6e966c6c94266eb39d09e6c4c4112572c
Lora Kisyova <llovchalieva@gmail.com>
2022-02-08 23:49:06 +0200

    Replace particles library as react-particles-js has been deprecated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Rank from './components/Rank/Rank';
 import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import Particles from 'react-particles-js';
+import Particles from 'react-tsparticles';
 import Clarifai from 'clarifai';
 
 const particlesOptions = {
@@ -18,7 +18,26 @@ const particlesOptions = {
           enable: true,
           value_area: 400
         }
-      }
+      },
+      size: {
+        value: 1,
+        random: true
+      },
+      move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 2,
+            straight: false,
+      },
+      links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          }
   }
 };
 
