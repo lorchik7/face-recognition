2aa91a95bf1e7da17592e89b64f877bb2cdab370
Lora Kisyova <llovchalieva@gmail.com>
2022-02-23 12:11:06 +0200

    Update database

--- src/App.js
+++ src/App.js
@@ -103,7 +103,7 @@ class App extends Component {
 	onButtonSubmit = () => {
 		this.setState({imageUrl: this.state.input});
 
-		fetch('http://localhost:3000/imageurl', {
+		fetch('https://floating-fjord-59639.herokuapp.com/imageurl', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
@@ -113,7 +113,7 @@ class App extends Component {
 		.then(response => response.json())
 		.then(response => {
 			if(response) {
-				fetch('http://localhost:3000/image', {
+				fetch('https://floating-fjord-59639.herokuapp.com:3000/image', {
 					method: 'put',
 					headers: {'Content-Type': 'application/json'},
 					body: JSON.stringify({
